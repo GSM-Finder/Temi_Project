df59440e45553b9331534b681aabc755da7f6fbd
harang <skltepone@gmail.com>
2022-11-11 12:25:37 +0900

    이동 위치 설정

--- app/src/main/java/com/sk3295/temicontroller/MainActivity.kt
+++ app/src/main/java/com/sk3295/temicontroller/MainActivity.kt
@@ -3,33 +3,23 @@ package com.sk3295.temicontroller
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.robotemi.sdk.Robot
-import com.robotemi.sdk.navigation.model.SpeedLevel
+import com.robotemi.sdk.*
+import com.robotemi.sdk.Robot.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.Executors
 
-private lateinit var robot: Robot
-
 private val executorService = Executors.newSingleThreadExecutor()
 
-
-class Robot {
-    private fun Robot.goTo(
-        location: String,
-        backwards: Boolean,
-        noBypass: Boolean,
-        speedLevel: SpeedLevel
-    ) {
-    }
-}
+private lateinit var robot: Robot
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        robot.goTo("1학년 1반")
 
         button.setOnClickListener {
             LocationText.setText("1-1로 이동 중 입니다.");
+            robot.goTo("1학년 1반")
         }
     }
 }
