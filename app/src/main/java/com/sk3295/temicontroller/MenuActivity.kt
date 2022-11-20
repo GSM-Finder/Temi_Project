d1117723e81eb71175a3cb567bbe541fa20ce907
Aubergine111 <hyeonjoon3668@gmail.com>
2022-11-20 15:34:22 +0900

    메인 UI 변경

---
+++ app/src/main/java/com/sk3295/temicontroller/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.sk3295.temicontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
